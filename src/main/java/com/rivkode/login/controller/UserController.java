21192c2ba5f4396031ecc829cf5499955795b8a4
jonghun <jonghuncu@gmail.com>
2023-04-09 20:13:31 +0900

    feat : Header 에 쿠키로 JWT 값 전달

--- src/main/java/com/rivkode/login/controller/UserController.java
+++ src/main/java/com/rivkode/login/controller/UserController.java
@@ -4,11 +4,14 @@ import com.rivkode.login.domain.dto.UserJoinRequest;
 import com.rivkode.login.domain.dto.UserLoginRequest;
 import com.rivkode.login.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,8 +27,22 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody UserLoginRequest dto) {
+    public ResponseEntity<String> login(@RequestBody UserLoginRequest dto, HttpServletResponse response) {
         String token = userService.login(dto.getUserName(), dto.getPassword());
-        return ResponseEntity.ok().body(token);
+        ResponseCookie cookie = ResponseCookie.from("ACCESSTOKEN", token)
+                .maxAge(7 * 24 * 60 * 60) // 쿠키 만료기간 7일
+                .path("/")
+                .secure(true)
+                .sameSite("None")
+                .httpOnly(true)
+                .build();
+
+        response.setHeader("Set-Cookie", cookie.toString());
+
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+
+
+        return new ResponseEntity<>(header, HttpStatus.OK);
     }
 }
